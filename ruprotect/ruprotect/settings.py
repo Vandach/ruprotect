e408458b3e3300525ab3341cd996a2144bebb1b4
Кирилл Аль-Шаер <kirill.al.shaer@gmail.com>
2026-02-04 18:14:28 +0300

    v2

--- ruprotect/ruprotect/settings.py
+++ ruprotect/ruprotect/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%zhy_vduvvgb&*@4wg696mnja=z8lm#(264oqga9ww9%4cpwfi'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
